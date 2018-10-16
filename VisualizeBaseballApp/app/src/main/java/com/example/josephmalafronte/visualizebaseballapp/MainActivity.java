3517eecc453f14de68ef85c6612f8b25255d3014
Unknown <jgmalafronte@gmail.com>
2018-10-16 15:34:27 -0500

    Successfully reading/writing to database

--- VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/MainActivity.java
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/MainActivity.java
@@ -7,14 +7,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,8 +36,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //Test write to database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference mDatabase = database.getReference();
+
+                DatabaseReference test = mDatabase.child("TestData").child("Value1");
+
+                test.setValue("Taco");
+
+                test.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String help = dataSnapshot.getValue().toString();
+                        Log.d("mytag",help);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("The read failed: " + databaseError.getCode());
+                    }
+                });
+
             }
         });
+
     }
 
     @Override
