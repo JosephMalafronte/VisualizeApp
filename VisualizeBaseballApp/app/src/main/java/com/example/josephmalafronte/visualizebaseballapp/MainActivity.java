b3dc879767580242ca11b6af390323e4cbaa5b0d
Unknown <jgmalafronte@gmail.com>
2018-10-22 16:15:45 -0500

    Working on adding a siteactivity

--- VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/MainActivity.java
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
--- VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
@@ -1,13 +1,19 @@
 package com.example.josephmalafronte.visualizebaseballapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TourActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tour);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
