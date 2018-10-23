1720c64ef6c375173398d2af831bc05d9d40db1a
Unknown <jgmalafronte@gmail.com>
2018-10-22 21:57:35 -0500

    Siteactivity improvements

---
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/SiteActivity.java
@@ -0,0 +1,52 @@
+package com.example.josephmalafronte.visualizebaseballapp;
+
+
+//Import Android Content
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+//Import Firebase Content
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.InputStream;
+
+
+
+public class SiteActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mDatabase = database.getReference();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+    }
+
+}
--- VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
@@ -5,15 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-public class TourActivity extends AppCompatActivity {
+public class TourActivity extends SiteActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-    }
+
 }
