e848c16817f7b288401628dec82ee461e6b249eb
Unknown <jgmalafronte@gmail.com>
2018-12-04 16:47:14 -0600

    Transfer Part Two

--- VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
+++ VisualizeBaseballApp/app/src/main/java/com/example/josephmalafronte/visualizebaseballapp/TourActivity.java
@@ -1,13 +1,20 @@
 package com.example.josephmalafronte.visualizebaseballapp;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class TourActivity extends AppCompatActivity {
+public class TourActivity extends SiteActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tour);
+    public void setMode() {
+        viewMode = 1;
     }
+
+
 }
