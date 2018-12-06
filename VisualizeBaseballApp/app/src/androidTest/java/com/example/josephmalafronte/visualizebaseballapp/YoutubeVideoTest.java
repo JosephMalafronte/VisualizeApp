9ea0b53ee5022260d3a00bc3ed0c1f159fcea348
Unknown <jgmalafronte@gmail.com>
2018-12-06 11:59:25 -0600

    More Tests

--- VisualizeBaseballApp/app/src/androidTest/java/com/example/josephmalafronte/visualizebaseballapp/YoutubeVideoTest.java
+++ VisualizeBaseballApp/app/src/androidTest/java/com/example/josephmalafronte/visualizebaseballapp/YoutubeVideoTest.java
@@ -1,29 +1,20 @@
 package com.example.josephmalafronte.visualizebaseballapp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import android.support.test.rule.ActivityTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-
-import static org.junit.Assert.assertEquals;
-
 @RunWith(AndroidJUnit4.class)
-public class GetSiteNameFromDatabaseTest  {
+public class YoutubeVideoTest {
 
     @Rule
     public ActivityTestRule<MainActivity> sActivityRule =
@@ -34,7 +25,7 @@ public class GetSiteNameFromDatabaseTest  {
     public void useAppContext() {
         // Context of the app under test.
         onView(withId(R.id.tourButton)).perform(click());
-        onView(withId(R.id.editText)).check(matches(withText("Locker Room")));
+        onView(withId(R.id.youtubePlayerView)).check(matches(withText("Youtube")));
     }
 
 }
